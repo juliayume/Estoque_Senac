e6ffe5318efcac52df3d2f6337b89a82556b2da3
juliayume <juliayumenk19@gmail.com>
2022-11-29 07:42:02 -0300

    29 nov commit

--- Estoque/Produtos/models.py
+++ Estoque/Produtos/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+from Local.models import Local
+from Categoria.models import Categoria
+from Fornecedor.models import Fornecedor
+from Status.models import Status
+
 # Create your models here.
+
+class Produto(models.Model):
+    id = models.AutoField(primary_key = True, unique = True)
+    nome = models.CharField(max_length = 100, null = False)
+    quantidade = models.IntegerField ()
+    local = models.ForeignKey(Local, on_delete = models.RESTRICT)
+    fornecedor = models.ForeignKey (Fornecedor, on_delete=models.RESTRICT)
+    categoria = models.ForeignKey (Categoria, on_delete=models.RESTRICT)
+    status = models.ForeignKey(Status, on_delete=models.RESTRICT)
